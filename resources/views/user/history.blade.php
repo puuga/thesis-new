48fe1ae632ebd7df3ee3926a517e26c645194194
puuga <siwaoh@gmail.com>
2015-07-29 14:55:06 +0700

    fixed bug no history

--- resources/views/user/history.blade.php
+++ resources/views/user/history.blade.php
@@ -40,7 +40,11 @@
 	<div class="row">
 
 		<div class="jumbotron">
-			<div id='calendar_div' style='width: 800; height: {{$year_count[0]->year_count*200}}px'></div>
+      @if (count($year_count) > 0)
+        <div id='calendar_div' style='width: 800; height: {{$year_count[0]->year_count*200}}px'></div>
+      @else
+        I don't have any records!
+      @endif
 		</div>
 
 		<h1>history count: {{ count($histories) }}</h1>
