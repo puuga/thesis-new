2cf80e95c1fcf21ada227f6465205a250a7b6c1f
puuga <siwaoh@gmail.com>
2015-08-01 14:38:58 +0700

    new contact page layout

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,62 +5,82 @@
 
 	<h1>Contact</h1>
 
-	<p>
-		<h3>SIWAWES WONGCHAROEN</h3>
+	<div class="row">
+	  <div class="col-xs-12 col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="//graph.facebook.com/1264549080/picture?type=large" alt="SIWAWES WONGCHAROEN">
+	      <div class="caption">
+	        <h3 class="text-center">SIWAWES WONGCHAROEN</h3>
+	        <p>
+						<dl class="">
+							<dt>Contact Address</dt>
+							<dd>
+								Department of Computer Science and Information Tecnology,
+								Faculty of Science, Naresuan Univesity, Phitsanulok, Thailand
+							</dd>
+						</dl>
 
-		<dl class="dl-horizontal">
-			<dt>Contact Address</dt>
-			<dd>
-				Department of Computer Science and Information Tecnology,
-				Faculty of Science, Naresuan Univesity, Phitsanulok, Thailand
-			</dd>
-		</dl>
+						<dl class="">
+							<dt>Email</dt>
+							<dd>
+								<a href="mailto:siwawesw55@email.nu.ac.th">siwawesw55@email.nu.ac.th</a>
+							</dd>
+						</dl>
+					</p>
+	      </div>
+	    </div>
+	  </div>
 
-		<dl class="dl-horizontal">
-			<dt>Email</dt>
-			<dd>
-				<a href="mailto:siwawesw55@email.nu.ac.th">siwawesw55@email.nu.ac.th</a>
-			</dd>
-		</dl>
-	</p>
+		<div class="col-xs-12 col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="//graph.facebook.com/647546174/picture?type=large" alt="JARATSRI RUNGRATTANAUBOL">
+	      <div class="caption">
+	        <h3 class="text-center">JARATSRI RUNGRATTANAUBOL</h3>
+	        <p>
+						<dl class="">
+							<dt>Contact Address</dt>
+							<dd>
+								Department of Computer Science and Information Tecnology,
+								Faculty of Science, Naresuan Univesity, Phitsanulok, Thailand
+							</dd>
+						</dl>
 
-	<p>
-		<h3>JARATSRI RUNGRATTANAUBOL</h3>
+						<dl class="">
+							<dt>Email</dt>
+							<dd>
+								<a href="mailto:jaratsrir@nu.ac.th">jaratsrir@nu.ac.th</a>
+							</dd>
+						</dl>
+					</p>
+	      </div>
+	    </div>
+	  </div>
 
-		<dl class="dl-horizontal">
-			<dt>Contact Address</dt>
-			<dd>
-				Department of Computer Science and Information Tecnology,
-				Faculty of Science, Naresuan Univesity, Phitsanulok, Thailand
-			</dd>
-		</dl>
+		<div class="col-xs-12 col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="//graph.facebook.com/510697185/picture?type=large" alt="ANTONY HARFIELD">
+	      <div class="caption">
+	        <h3 class="text-center">ANTONY HARFIELD</h3>
+	        <p>
+						<dl class="">
+							<dt>Contact Address</dt>
+							<dd>
+								Department of Computer Science and Information Tecnology,
+								Faculty of Science, Naresuan Univesity, Phitsanulok, Thailand
+							</dd>
+						</dl>
 
-		<dl class="dl-horizontal">
-			<dt>Email</dt>
-			<dd>
-				<a href="mailto:jaratsrir@nu.ac.th">jaratsrir@nu.ac.th</a>
-			</dd>
-		</dl>
-	</p>
-
-	<p>
-		<h3>ANTONY HARFIELD</h3>
-
-		<dl class="dl-horizontal">
-			<dt>Contact Address</dt>
-			<dd>
-				Department of Computer Science and Information Tecnology,
-				Faculty of Science, Naresuan Univesity, Phitsanulok, Thailand
-			</dd>
-		</dl>
-
-		<dl class="dl-horizontal">
-			<dt>Email</dt>
-			<dd>
-				<a href="mailto:antonyh@nu.ac.th">antonyh@nu.ac.th</a>
-			</dd>
-		</dl>
-	</p>
+						<dl class="">
+							<dt>Email</dt>
+							<dd>
+								<a href="mailto:antonyh@nu.ac.th">antonyh@nu.ac.th</a>
+							</dd>
+						</dl>
+					</p>
+	      </div>
+	    </div>
+	  </div>
+	</div>
 
 </div>
 @endsection
