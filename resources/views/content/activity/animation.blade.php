29cd9719de902ee74823808429bd2d1a83549288
puuga <siwaoh@gmail.com>
2015-08-11 10:45:37 +0700

    add preview audio on activity design page.

--- resources/views/content/activity/animation.blade.php
+++ resources/views/content/activity/animation.blade.php
@@ -12,6 +12,12 @@
           {{ $activity->correct_animation === '1' ? 'checked' : '' }}>
           Set 1
         </label>
+        <audio controls>
+          <source
+            src="{{ asset('/sounds/FFXIV_Victory_Fanfare.ogg') }}"
+            type="audio/ogg">
+            Your browser does not support the audio element.
+        </audio>
       </div>
       <div class="radio radio-primary">
         <label>
@@ -23,13 +29,19 @@
           {{ $activity->correct_animation === '2' ? 'checked' : '' }}>
           Set 2
         </label>
+        <audio controls>
+      	  <source
+            src="{{ asset('/sounds/d2ef5d_Angry_Birds_Level_Complete_Sound_Effect.mp3') }}"
+        		type="audio/mpeg">
+      	  Your browser does not support the audio element.
+      	</audio>
       </div>
     </div>
   </div>
 
   <div class="form-group">
     <label class="col-xs-2 control-label">Incorrect animation</label>
-    <div class="col-xs-3">
+    <div class="col-xs-10">
       <div class="radio radio-primary">
         <label>
           <input
@@ -40,6 +52,12 @@
           {{ $activity->correct_animation === '1' ? 'checked' : '' }}>
           Set 1
         </label>
+        <audio controls>
+      	  <source
+            src="{{ asset('/sounds/Sad_Trombone-Joe_Lamb-665429450.mp3') }}"
+        		type="audio/mpeg">
+      	  Your browser does not support the audio element.
+      	</audio>
       </div>
       <div class="radio radio-primary">
         <label>
@@ -51,6 +69,12 @@
           {{ $activity->correct_animation === '2' ? 'checked' : '' }}>
           Set 2
         </label>
+        <audio controls>
+      	  <source
+            src="{{ asset('/sounds/e264ad_Angry_Birds_Level_Failed_Piglets_Sound_Effect.mp3') }}"
+        		type="audio/mpeg">
+      	  Your browser does not support the audio element.
+      	</audio>
       </div>
     </div>
   </div>
