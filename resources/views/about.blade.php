3a52715b1d7ebffdbcf1ae65246d91a92bbd6589
puuga <siwaoh@gmail.com>
2015-09-04 13:03:51 +0700

    add content to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,7 +3,71 @@
 @section('content')
 <div class="container">
 
-	<h1>About</h1>
+	<h1>
+		About<br/>
+		<small>
+			This web-application is part of
+			"A Platform-Independent Framework for Authoring and Monitoring Contents on Tablets"
+			research.
+		</small>
+	</h1>
+
+	<div class="panel panel-info">
+	  <div class="panel-heading">
+	    <h3 class="panel-title">
+				<small>Article 1</small>
+				A Framework for Empowering Teachers to Author Interactive Content for Tablet Classroom Activities
+			</h3>
+	  </div>
+	  <div class="panel-body">
+	    <p>
+	    	Abstract— This paper proposes a framework that is aimed at empowering teachers to create interactive content on tablets to support students’ learning activities in the classroom. Tablets are becoming widely used in schools to support student learning and to motivate extra class activities. In contrast to traditional tools such as pen and paper, one of the most significant drawbacks of the current era is that teachers do not have the skills to create interactive content for tablets by themselves. The framework described in this paper makes it easier for teachers to design content that is interactive and implement it on a number of platforms.
+	    </p>
+	  </div>
+		<div class="panel-footer">
+			Keywords— tablets; interactive content; authoring and monitoring tool; students’ learning interactivity
+		</div>
+	</div>
+
+	<div class="panel panel-primary">
+	  <div class="panel-heading">
+			<h3 class="panel-title">Introduction</h3>
+		</div>
+	  <div class="panel-body">
+			<p>
+				Computer and technology has become an essential tool to drive almost all activities in every organization, including educational organizations such as schools, colleges and universities. From past to present, there are many frameworks and technology tools to support education, such as CAI, Intelligent Tutoring Systems (Kazi, 2004) and E-learning. E-learning is one of the current successful educational tools, which have been used globally. Various approaches in educational technology have been proposed to support both students’ self-learning and classroom. One-Man University in China offer online courses to Chinese (Economist, 2014) with objective to make everyone equal. In Korea, The Korean Advanced Institute of Science and Technology (KAIST) has introduced Education 3.0 model which combined interactive class and online self-learning. The main idea of education 3.0 is anytime, anywhere and interactive with environment (FacultyEcommons, 2013).
+			</p>
+			<p>
+				Most schools and universities in many countries have used e-learning for self-learning and self-access classroom, and some offered online-degree. After emerging of smart phones and tablets, with their ease of use and advanced communication technology e.g. Wi-Fi, most people around the world have become addicted to tablets and mobile phones. The increasing popularity of using tablets and mobiles have also affected a way to support student’s learning and to motivate some learning activities for students, for instance, UAE: United Arab Emirates (Gulfnews, 2013), UK: United Kingdoms (BBC, 2014) even in USA: united stage of America (The Economist, 2014) are the countries that provide tablets and use of tablet in class. Hence, many research and studies have been proposed and implemented to support tablet uses in education. The results from many studies related to the use of tablet in class have shown that students have more positive feedback and intention on learning with two-way-interactive tablet comparing to an ordinary method such as listen to a lecture (Li, 2010). These results prove that using tablet in class brings good effects for students.
+			</p>
+			<p>
+				In 2012, the Thai government launched a project called “One Tablet Per Child” (OTPC) and handing out tablets to all primary school students. However, in the initial concentration on overcoming hardware, software and management issues, there us yet to be significant research in understanding how best to make use of the tablets in a classroom environment. By giving tablets with built-in applications and contents to every first-year-students in primary schools, teachers have to use all materials based on this built-in. Most of built-in applications have routines and basic learning patterns i.e. contents, quizzes and score summary, which may not directly response to what teachers’ requirements. All tablets are based on Android OS platform. Therefore, this may restrict the number of applications and learning topics and contents implemented with other platforms, such as iOS, Windows mobiles and etc. This limitation is contrary to the initial objectives of the project OTPC, which aims to increase opportunity and equality of access to education (Blognone, 2012)
+			</p>
+			<p>
+				Moreover, the most significant drawback of current learning applications is lacking of standard guidelines on teaching issue, for instance, the design of the learning alignments, the contents (Kaewkiriya, 2010), and the evaluation methods to monitor students’ interactivity (Harfield, 2013). Moreover, all those applications are also restricted to one operation on only one platform.
+			</p>
+			<p>
+				In the past, teachers led innovations in learning by developing their own content and activities on computers, when teachers could write their own programs for use in a class. Nowadays, with individual app on tablets, this is made more difficult or even impossible for teachers, because of the high standard of commercially developed applications and the high level of skills required to develop new applications (Selwyn, 2002).
+			</p>
+			<p>
+				With these problems mentioned above, it is very challenge to design a way to overcome. Such problems can be summarized as a limitation of platform, a limitation of developing teachers’ own interactive content and a limitation of teachers’ monitoring and evaluations. Therefore, this research proposes a framework to support classroom environment based on those limitations. A guideline of creating the applications or learning content, which work on multiple platforms, and provides a way to monitoring students’ interactivity, should be initiated. Such information from monitoring can help teachers or administrators to support and improve in students' interactive.
+			</p>
+		</div>
+	</div>
+
+	<div class="panel panel-primary">
+	  <div class="panel-heading">
+	    <h3 class="panel-title">Objectives</h3>
+	  </div>
+	  <div class="panel-body">
+	    <ol>
+	    	<li>To design a framework for authoring interactive contents on Tablets.</li>
+				<li>To implement a platform-independent environment for authoring contents and publishing them based on HTML5 and JavaScript.</li>
+				<li>To create a prototype with the contents, for first year primary school students, created from the framework, then collecting the feedbacks and evaluate the framework.</li>
+	    </ol>
+	  </div>
+	</div>
+
 
 </div>
 @endsection
